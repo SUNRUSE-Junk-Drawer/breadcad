33f4d0ebefb9a144f0265184f6caa3fd1f31d7d4
jameswilddev <jameswilddev@outlook.com>
2019-09-20 22:53:45 +0100

    Add read functions.

---
+++ src/framework/read.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include "fail.h"
+#include "types.h"
+
+#define SDF_READ(type)                                       \
+  sdf_boolean_t sdf_read_##type##_or_eof(                    \
+    FILE * file,                                             \
+    const char * what,                                       \
+    sdf_##type##_t * value                                   \
+  ) {                                                        \
+    switch (fread(value, sizeof(sdf_##type##_t), 1, file)) { \
+      case 0:                                                \
+        if (!feof(stdin)) {                                  \
+          sdf_fail(                                          \
+            "error reading %s (ferror: %d)\n",               \
+            what,                                            \
+            ferror(stdin)                                    \
+          );                                                 \
+        }                                                    \
+        return SDF_BOOLEAN_FALSE;                            \
+                                                             \
+      case 1:                                                \
+        sdf_types_##type##_swap_endianness(value);           \
+        return SDF_BOOLEAN_TRUE;                             \
+                                                             \
+      /* I don't think this can happen. */                   \
+      default:                                               \
+        sdf_fail(                                            \
+          "error reading %s (ferror: %d, feof: %d)\n",       \
+          what,                                              \
+          ferror(stdin),                                     \
+          feof(stdin)                                        \
+        );                                                   \
+                                                             \
+        /* Needed to satisfy compiler;             */        \
+        /* exit(...) is always called before this. */        \
+        return SDF_BOOLEAN_FALSE;                            \
+    }                                                        \
+  }                                                          \
+                                                             \
+  sdf_##type##_t sdf_read_##type(                            \
+    FILE * file,                                             \
+    const char * what                                        \
+  ) {                                                        \
+    sdf_##type##_t output;                                   \
+    if (!sdf_read_##type##_or_eof(file, what, &output)) {    \
+      sdf_fail("unexpected eof reading %s\n", what);         \
+    }                                                        \
+    return output;                                           \
+  }
+
+SDF_READ(u16)
+SDF_READ(f32)
---
+++ src/framework/read.h
@@ -0,0 +1,22 @@
+#ifndef SDF_READ_H
+#define SDF_READ_H
+
+#include <stdio.h>
+#include "types.h"
+
+#define SDF_READ_TYPE(type)               \
+  sdf_boolean_t sdf_read_##type##_or_eof( \
+    FILE * file,                          \
+    const char * what,                    \
+    sdf_##type##_t * value                \
+  );                                      \
+                                          \
+  sdf_##type##_t sdf_read_##type(         \
+    FILE * file,                          \
+    const char * what                     \
+  );
+
+SDF_READ_TYPE(u16)
+SDF_READ_TYPE(f32)
+
+#endif
