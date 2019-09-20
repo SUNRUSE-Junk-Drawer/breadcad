03292d2b417a8a72714d01a720ea124f691baf17
jameswilddev <jameswilddev@outlook.com>
2019-09-20 06:35:07 +0100

    Add float CLI argument parsing.

--- src/framework/cli.c
+++ src/framework/cli.c
@@ -1,8 +1,130 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include "fail.h"
+#include "malloc.h"
+#include "types.h"
 #include "executable.h"
 
+static int sdf__argc;
+static char ** sdf__argv;
+static sdf_boolean_t * sdf__used;
+static int * sdf__lengths;
+
+static int sdf__find_index(
+  const char * short_name,
+  const char * long_name
+) {
+  int argument = 0;
+  while (argument < sdf__argc) {
+    if (!sdf__used[argument]) {
+      if (sdf__lengths[argument] > 1 && sdf__argv[argument][0] == '-') {
+        if (!strcmp(&sdf__argv[argument][1], short_name)) {
+          sdf__used[argument] = SDF_BOOLEAN_TRUE;
+          return argument;
+        }
+
+        if (sdf__lengths[argument] > 2 && sdf__argv[argument][1] == '-') {
+          if (!strcmp(&sdf__argv[argument][2], long_name)) {
+            sdf__used[argument] = SDF_BOOLEAN_TRUE;
+            return argument;
+          }
+        }
+      }
+    }
+    argument++;
+  }
+  return -1;
+}
+
+static const char * sdf__find_value(
+  const char * short_name,
+  const char * long_name
+) {
+  int argument = sdf__find_index(short_name, long_name);
+
+  if (argument == -1) {
+    return NULL;
+  }
+
+  argument++;
+
+  if (argument == sdf__argc || sdf__used[argument]) {
+    sdf_fail(
+      "expected a value for command line argument -%s/--%s\n",
+      short_name,
+      long_name
+    );
+  }
+
+  sdf__used[argument] = SDF_BOOLEAN_TRUE;
+  return sdf__argv[argument];
+}
+
+void sdf_cli_float(
+  const char * short_name,
+  const char * long_name,
+  const char * description,
+  sdf_f32_t * pointer_to_result,
+  sdf_f32_t default_value
+) {
+  const char * value;
+  char waste;
+
+  value = sdf__find_value(short_name, long_name);
+
+  if (value) {
+    if (sscanf(value, "%f%c", pointer_to_result, &waste) != 1) {
+      sdf_fail(
+        "unable to parse command line argument -%s/--%s value \"%s\" as a float\n",
+        short_name,
+        long_name,
+        value
+      );
+    }
+
+    return;
+  }
+
+  * pointer_to_result = default_value;
+}
+
+static void sdf__create_used(void) {
+  int argument = 0;
+
+  sdf__used = SDF_MALLOC(
+    sdf_boolean_t,
+    sdf__argc,
+    "track which command line arguments have been used"
+  );
+
+  sdf__used[0] = SDF_BOOLEAN_TRUE;
+  sdf__used[1] = SDF_BOOLEAN_TRUE;
+
+  while (argument < sdf__argc) {
+    sdf__used[argument] = SDF_BOOLEAN_FALSE;
+    argument++;
+  }
+}
+
+static void sdf__create_lengths(void) {
+  int argument = 0;
+
+  sdf__lengths = SDF_MALLOC(
+    int,
+    sdf__argc,
+    "track the lengths of the command line arguments"
+  );
+
+  while (argument < sdf__argc) {
+    sdf__lengths[argument] = strlen(sdf__argv[argument]);
+    argument++;
+  }
+}
+
 void sdf_cli(int argc, char * argv[]) {
+  sdf__argc = argc;
+  sdf__argv = argv;
   printf("%s - %s\n", sdf_executable_name, sdf_executable_description);
   printf(
     "  usage: %s%s [options]%s\n",
@@ -10,6 +132,8 @@ void sdf_cli(int argc, char * argv[]) {
     sdf_executable_name,
     sdf_executable_usage_suffix
   );
+  sdf__create_used();
+  sdf__create_lengths();
   sdf_executable_cli();
   exit(0);
 }
--- src/framework/cli.h
+++ src/framework/cli.h
@@ -1,6 +1,14 @@
 #ifndef SDF_CLI_H
 #define SDF_CLI_H
 
+void sdf_cli_float(
+  const char * short_name,
+  const char * long_name,
+  const char * description,
+  sdf_f32_t * pointer_to_result,
+  sdf_f32_t default_value
+);
+
 void sdf_cli(int argc, char * argv[]);
 
 #endif
