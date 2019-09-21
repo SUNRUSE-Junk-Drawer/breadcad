ab505a5990f096277113bef8d7d8c18e66314e61
jameswilddev <jameswilddev@outlook.com>
2019-09-21 10:00:46 +0100

    Experiment to try and figure out why Windows builds are broken.

--- setup.bash
+++ setup.bash
@@ -1,6 +1,6 @@
 if [[ "$OSTYPE" == "win32" ]]; then
   export SDF_BATS_PATH=../bin/bats
-  export SDF_LINE_BREAK=$'\r\n'
+  export SDF_LINE_BREAK=$'Before\r\nAfter'
 else
   SDF_BATS_PATH=submodules/bats-core/bin/bats
   export SDF_BATS_PATH
