1b14a18cfc1da998933cab53e1098ce5e734d7d9
jameswilddev <jameswilddev@outlook.com>
2019-09-22 18:27:48 +0100

    Implement checked realloc.

--- src/framework/malloc.c
+++ src/framework/malloc.c
@@ -8,16 +8,44 @@ void * sdf_malloc(
   const char * action
 ) {
   if (size) {
-  void * allocated = malloc(size);
-  if (!allocated) {
-    sdf_fail(
-      "failed to allocate %u bytes to %s\n",
-      (unsigned long) size,
-      action
-    );
-  }
-  return allocated;
+    void * allocated = malloc(size);
+    if (!allocated) {
+      sdf_fail(
+        "failed to allocate %u bytes to %s\n",
+        (unsigned long) size,
+        action
+      );
+    }
+    return allocated;
   } else {
     return NULL;
+  }
+}
+
+void sdf_realloc(
+  size_t size,
+  const char * action,
+  void ** pointer_to_pointer
+) {
+  void * resized;
+  if (size) {
+    if (*pointer_to_pointer) {
+      resized = realloc(*pointer_to_pointer, size);
+      if (!resized) {
+        sdf_fail(
+          "failed to reallocate %u bytes to %s\n",
+          (unsigned long) size,
+          action
+        );
+      }
+      *pointer_to_pointer = resized;
+    } else {
+      *pointer_to_pointer = sdf_malloc(size, action);
+    }
+  } else {
+    if (*pointer_to_pointer) {
+      free(*pointer_to_pointer);
+      *pointer_to_pointer = NULL;
+    }
   }
 }
--- src/framework/malloc.h
+++ src/framework/malloc.h
@@ -9,4 +9,13 @@ void * sdf_malloc(
 #define SDF_MALLOC(type, quantity, action) \
   ((type * ) sdf_malloc((quantity) * sizeof(type), action))
 
+void sdf_realloc(
+  size_t size,
+  const char * action,
+  void ** pointer_to_pointer
+);
+
+#define SDF_REALLOC(type, quantity, action, pointer_to_pointer) \
+  sdf_realloc((quantity) * sizeof(type), action, (void**) &(pointer_to_pointer))
+
 #endif
