980516b49e35d38cf51b40bbf35cd40b155754db
jameswilddev <jameswilddev@outlook.com>
2019-09-22 10:40:44 +0100

    Assist static analysis (was unsure whether local variables always assigned).

--- src/framework/read_sdf.c
+++ src/framework/read_sdf.c
@@ -10,40 +10,31 @@ void sdf_read_sdf(
   FILE * file
 ) {
   sdf_opcode_t opcode;
-  sdf_opcode_arity_t arity;
   sdf_pointer_t argument_a;
   sdf_pointer_t argument_b;
   sdf_pointer_t argument_c;
 
   while (sdf_read_u16_or_eof(file, "opcode", &opcode)) {
-    arity = sdf_opcode_arity(opcode);
-
-    if (arity) {
-      argument_a = sdf_read_u16(file, "argument a");
-    }
-
-    if (arity > 1) {
-      argument_b = sdf_read_u16(file, "argument b");
-    }
-
-    if (arity > 2) {
-      argument_c = sdf_read_u16(file, "argument c");
-    }
-
-    switch (arity) {
+    switch (sdf_opcode_arity(opcode)) {
       case 0:
         sdf_executable_nullary(opcode);
         break;
 
       case 1:
+        argument_a = sdf_read_u16(file, "argument a");
         sdf_executable_unary(opcode, argument_a);
         break;
 
       case 2:
+        argument_a = sdf_read_u16(file, "argument a");
+        argument_b = sdf_read_u16(file, "argument b");
         sdf_executable_binary(opcode, argument_a, argument_b);
         break;
 
       case 3:
+        argument_a = sdf_read_u16(file, "argument a");
+        argument_b = sdf_read_u16(file, "argument b");
+        argument_c = sdf_read_u16(file, "argument c");
         sdf_executable_ternary(opcode, argument_a, argument_b, argument_c);
         break;
     }
