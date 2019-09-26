aab29b19aa33d4368056054490d1e00db703e7b2
jameswilddev <jameswilddev@outlook.com>
2019-09-26 07:02:44 +0100

    Change sdf_read_(type)_or_eof to raise an error if the file is not empty but does not contain enough bytes to complete the requested type.

--- src/framework/read.c
+++ src/framework/read.c
@@ -3,51 +3,54 @@
 #include "types.h"
 #include "read.h"
 
-#define SDF_READ(type)                                       \
-  sdf_boolean_t sdf_read_##type##_or_eof(                    \
-    FILE * file,                                             \
-    const char * what,                                       \
-    sdf_##type##_t * value                                   \
-  ) {                                                        \
-    switch (fread(value, sizeof(sdf_##type##_t), 1, file)) { \
-      case 0:                                                \
-        if (!feof(stdin)) {                                  \
-          sdf_fail(                                          \
-            "error reading %s (ferror: %d)\n",               \
-            what,                                            \
-            ferror(stdin)                                    \
-          );                                                 \
-        }                                                    \
-        return SDF_BOOLEAN_FALSE;                            \
-                                                             \
-      case 1:                                                \
-        sdf_types_##type##_swap_endianness(value);           \
-        return SDF_BOOLEAN_TRUE;                             \
-                                                             \
-      /* I don't think this can happen. */                   \
-      default:                                               \
-        sdf_fail(                                            \
-          "error reading %s (ferror: %d, feof: %d)\n",       \
-          what,                                              \
-          ferror(stdin),                                     \
-          feof(stdin)                                        \
-        );                                                   \
-                                                             \
-        /* Needed to satisfy compiler;             */        \
-        /* exit(...) is always called before this. */        \
-        return SDF_BOOLEAN_FALSE;                            \
-    }                                                        \
-  }                                                          \
-                                                             \
-  sdf_##type##_t sdf_read_##type(                            \
-    FILE * file,                                             \
-    const char * what                                        \
-  ) {                                                        \
-    sdf_##type##_t output;                                   \
-    if (!sdf_read_##type##_or_eof(file, what, &output)) {    \
-      sdf_fail("unexpected eof reading %s\n", what);         \
-    }                                                        \
-    return output;                                           \
+#define SDF_READ(type)                                              \
+  sdf_boolean_t sdf_read_##type##_or_eof(                           \
+    FILE * file,                                                    \
+    const char * what,                                              \
+    sdf_##type##_t * value                                          \
+  ) {                                                               \
+    size_t bytes = fread(value, 1, sizeof(sdf_##type##_t), file);   \
+    if (bytes < sizeof(sdf_##type##_t)) {                           \
+      if (!feof(stdin)) {                                           \
+        sdf_fail(                                                   \
+          "error reading %s (ferror: %d)\n",                        \
+          what,                                                     \
+          ferror(stdin)                                             \
+        );                                                          \
+        /* Required to satisfy compiler; will always exit above. */ \
+        return SDF_BOOLEAN_FALSE;                                   \
+      } else if (bytes) {                                           \
+        sdf_fail("unexpected eof reading %s\n", what);              \
+        /* Required to satisfy compiler; will always exit above. */ \
+        return SDF_BOOLEAN_FALSE;                                   \
+      } else {                                                      \
+        return SDF_BOOLEAN_FALSE;                                   \
+      }                                                             \
+    } else if (bytes > sizeof(sdf_##type##_t)) {                    \
+      /* I don't think this can happen. */                          \
+      sdf_fail(                                                     \
+        "error reading %s (ferror: %d, feof: %d)\n",                \
+        what,                                                       \
+        ferror(stdin),                                              \
+        feof(stdin)                                                 \
+      );                                                            \
+      /* Required to satisfy compiler; will always exit above. */   \
+      return SDF_BOOLEAN_FALSE;                                     \
+    } else {                                                        \
+      sdf_types_##type##_swap_endianness(value);                    \
+      return SDF_BOOLEAN_TRUE;                                      \
+    }                                                               \
+  }                                                                 \
+                                                                    \
+  sdf_##type##_t sdf_read_##type(                                   \
+    FILE * file,                                                    \
+    const char * what                                               \
+  ) {                                                               \
+    sdf_##type##_t output;                                          \
+    if (!sdf_read_##type##_or_eof(file, what, &output)) {           \
+      sdf_fail("unexpected eof reading %s\n", what);                \
+    }                                                               \
+    return output;                                                  \
   }
 
 SDF_READ(u16)
