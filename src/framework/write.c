4347cd775cc5fc07f43a9e733425b554cabf78c0
jameswilddev <jameswilddev@outlook.com>
2019-09-27 06:19:29 +0100

    Add writing helpers.

---
+++ src/framework/write.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "fail.h"
+#include "types.h"
+#include "write.h"
+
+#define SDF_WRITE(type)                                                    \
+  void sdf_write_##type(                                                   \
+    FILE * file,                                                           \
+    sdf_##type##_t value,                                                  \
+    const char * what                                                      \
+  ) {                                                                      \
+    sdf_##type##_t copy = value;                                           \
+    sdf_types_##type##_swap_endianness(&copy);                             \
+    if (!fwrite(&copy, sizeof(sdf_##type##_t), 1, file)) {                 \
+      sdf_fail("failed to write %s (ferror: %d)\n", what, ferror(file));   \
+    }                                                                      \
+  }
+
+SDF_WRITE(u16)
+SDF_WRITE(f32)
---
+++ src/framework/write.h
@@ -0,0 +1,17 @@
+#ifndef SDF_WRITE_H
+#define SDF_WRITE_H
+
+#include <stdio.h>
+#include "types.h"
+
+#define SDF_WRITE_TYPE(type) \
+  void sdf_write_##type(     \
+    FILE * file,             \
+    sdf_##type##_t type,     \
+    const char * what        \
+  );
+
+SDF_WRITE_TYPE(u16)
+SDF_WRITE_TYPE(f32)
+
+#endif
