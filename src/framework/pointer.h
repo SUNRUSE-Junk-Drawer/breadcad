26139f3821b0ff8d5178e8711dec1d268a405ce1
jameswilddev <jameswilddev@outlook.com>
2019-09-28 08:11:01 +0100

    Altered pointer type to support float constants (#11).

--- src/framework/pointer.h
+++ src/framework/pointer.h
@@ -4,7 +4,8 @@
 #include "types.h"
 
 typedef sdf_u16_t sdf_pointer_t;
-#define SDF_POINTER_MAX SDF_U16_MAX
-#define SDF_POINTER_RANGE SDF_U16_RANGE
+#define SDF_POINTER_MAX (SDF_U16_MAX - 1)
+#define SDF_POINTER_RANGE (SDF_U16_RANGE - 1)
+#define SDF_POINTER_FLOAT_CONSTANT SDF_U16_MAX
 
 #endif
