7a7f61279324d2af258186137098889934ae4580
jameswilddev <jameswilddev@outlook.com>
2019-10-10 18:56:53 +0100

    Add support for float constants when executing ternary operators (#11).

--- src/framework/execute.c
+++ src/framework/execute.c
@@ -88,15 +88,99 @@
 #define SDF_EXECUTE_TERNARY(name, argument_a_type, argument_b_type, argument_c_type, result_type, implementation) \
   case SDF_OPCODE_##name:                                                                                         \
     result_buffer_##result_type = buffers[sdf_plan_result_buffers[instruction]];                                  \
-    argument_a_buffer_##argument_a_type = buffers[sdf_plan_argument_buffers[(*argument)++]];                      \
-    argument_b_buffer_##argument_b_type = buffers[sdf_plan_argument_buffers[(*argument)++]];                      \
-    argument_c_buffer_##argument_c_type = buffers[sdf_plan_argument_buffers[(*argument)++]];                      \
-    while (iteration < iterations) {                                                                              \
-      argument_a_##argument_a_type = argument_a_buffer_##argument_a_type[iteration];                              \
-      argument_b_##argument_b_type = argument_b_buffer_##argument_b_type[iteration];                              \
-      argument_c_##argument_c_type = argument_c_buffer_##argument_c_type[iteration];                              \
-      result_buffer_##result_type[iteration] = implementation;                                                    \
-      iteration++;                                                                                                \
+    if (sdf_store_argument_pointers[*argument] == SDF_POINTER_FLOAT_CONSTANT) {                                   \
+      argument_a_##argument_a_type = sdf_store_argument_float_constants[*argument];                               \
+      (*argument)++;                                                                                              \
+      if (sdf_store_argument_pointers[*argument] == SDF_POINTER_FLOAT_CONSTANT) {                                 \
+        argument_b_##argument_b_type = sdf_store_argument_float_constants[*argument];                             \
+        (*argument)++;                                                                                            \
+        if (sdf_store_argument_pointers[*argument] == SDF_POINTER_FLOAT_CONSTANT) {                               \
+          argument_c_##argument_c_type = sdf_store_argument_float_constants[*argument];                           \
+          (*argument)++;                                                                                          \
+          result_##result_type = implementation;                                                                  \
+          while (iteration < iterations) {                                                                        \
+            result_buffer_##result_type[iteration] = result_##result_type;                                        \
+            iteration++;                                                                                          \
+          }                                                                                                       \
+        } else {                                                                                                  \
+          argument_c_buffer_##argument_c_type = buffers[sdf_plan_argument_buffers[*argument]];                    \
+          (*argument)++;                                                                                          \
+          while (iteration < iterations) {                                                                        \
+            argument_c_##argument_c_type = argument_c_buffer_##argument_c_type[iteration];                        \
+            result_buffer_##result_type[iteration] = implementation;                                              \
+            iteration++;                                                                                          \
+          }                                                                                                       \
+        }                                                                                                         \
+      } else {                                                                                                    \
+        argument_b_buffer_##argument_b_type = buffers[sdf_plan_argument_buffers[*argument]];                      \
+        (*argument)++;                                                                                            \
+        if (sdf_store_argument_pointers[*argument] == SDF_POINTER_FLOAT_CONSTANT) {                               \
+          argument_c_##argument_c_type = sdf_store_argument_float_constants[*argument];                           \
+          (*argument)++;                                                                                          \
+          while (iteration < iterations) {                                                                        \
+            argument_b_##argument_b_type = argument_b_buffer_##argument_b_type[iteration];                        \
+            result_buffer_##result_type[iteration] = implementation;                                              \
+            iteration++;                                                                                          \
+          }                                                                                                       \
+        } else {                                                                                                  \
+          argument_c_buffer_##argument_c_type = buffers[sdf_plan_argument_buffers[*argument]];                    \
+          (*argument)++;                                                                                          \
+          while (iteration < iterations) {                                                                        \
+            argument_b_##argument_b_type = argument_b_buffer_##argument_b_type[iteration];                        \
+            argument_c_##argument_c_type = argument_c_buffer_##argument_c_type[iteration];                        \
+            result_buffer_##result_type[iteration] = implementation;                                              \
+            iteration++;                                                                                          \
+          }                                                                                                       \
+        }                                                                                                         \
+      }                                                                                                           \
+    } else {                                                                                                      \
+      argument_a_buffer_##argument_a_type = buffers[sdf_plan_argument_buffers[*argument]];                        \
+      (*argument)++;                                                                                              \
+      if (sdf_store_argument_pointers[*argument] == SDF_POINTER_FLOAT_CONSTANT) {                                 \
+        argument_b_##argument_b_type = sdf_store_argument_float_constants[*argument];                             \
+        (*argument)++;                                                                                            \
+        if (sdf_store_argument_pointers[*argument] == SDF_POINTER_FLOAT_CONSTANT) {                               \
+          argument_c_##argument_c_type = sdf_store_argument_float_constants[*argument];                           \
+          (*argument)++;                                                                                          \
+          while (iteration < iterations) {                                                                        \
+            argument_a_##argument_a_type = argument_a_buffer_##argument_a_type[iteration];                        \
+            result_buffer_##result_type[iteration] = implementation;                                              \
+            iteration++;                                                                                          \
+          }                                                                                                       \
+        } else {                                                                                                  \
+          argument_c_buffer_##argument_c_type = buffers[sdf_plan_argument_buffers[*argument]];                    \
+          (*argument)++;                                                                                          \
+          while (iteration < iterations) {                                                                        \
+            argument_a_##argument_a_type = argument_a_buffer_##argument_a_type[iteration];                        \
+            argument_c_##argument_c_type = argument_c_buffer_##argument_c_type[iteration];                        \
+            result_buffer_##result_type[iteration] = implementation;                                              \
+            iteration++;                                                                                          \
+          }                                                                                                       \
+        }                                                                                                         \
+      } else {                                                                                                    \
+        argument_b_buffer_##argument_b_type = buffers[sdf_plan_argument_buffers[*argument]];                      \
+        (*argument)++;                                                                                            \
+        if (sdf_store_argument_pointers[*argument] == SDF_POINTER_FLOAT_CONSTANT) {                               \
+          argument_c_##argument_c_type = sdf_store_argument_float_constants[*argument];                           \
+          (*argument)++;                                                                                          \
+          while (iteration < iterations) {                                                                        \
+            argument_a_##argument_a_type = argument_a_buffer_##argument_a_type[iteration];                        \
+            argument_b_##argument_b_type = argument_b_buffer_##argument_b_type[iteration];                        \
+            result_buffer_##result_type[iteration] = implementation;                                              \
+            iteration++;                                                                                          \
+          }                                                                                                       \
+        } else {                                                                                                  \
+          argument_c_buffer_##argument_c_type = buffers[sdf_plan_argument_buffers[*argument]];                    \
+          (*argument)++;                                                                                          \
+          while (iteration < iterations) {                                                                        \
+            argument_a_##argument_a_type = argument_a_buffer_##argument_a_type[iteration];                        \
+            argument_b_##argument_b_type = argument_b_buffer_##argument_b_type[iteration];                        \
+            argument_c_##argument_c_type = argument_c_buffer_##argument_c_type[iteration];                        \
+            result_buffer_##result_type[iteration] = implementation;                                              \
+            iteration++;                                                                                          \
+          }                                                                                                       \
+        }                                                                                                         \
+      }                                                                                                           \
     }                                                                                                             \
     break;
 
