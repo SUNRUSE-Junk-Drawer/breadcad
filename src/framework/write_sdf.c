8251dea370cb1a63c53088217570a1d62f4caa19
jameswilddev <jameswilddev@outlook.com>
2019-10-13 22:21:58 +0100

    Redesign SDF writing to use encapsulated arguments (#32).

--- src/framework/write_sdf.c
+++ src/framework/write_sdf.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "opcode.h"
 #include "pointer.h"
+#include "argument.h"
 #include "write.h"
 #include "write_sdf.h"
 
@@ -8,65 +9,64 @@ static sdf_pointer_t sdf__written = 0;
 
 static void sdf__write_argument(
   FILE * file,
-  sdf_pointer_t pointer,
-  sdf_f32_t float_constant
+  sdf_argument_t argument
 ) {
-  sdf_write_u16(file, pointer, "argument pointer");
-  if (pointer == SDF_POINTER_FLOAT_CONSTANT) {
-    sdf_write_f32(file, float_constant, "argument float constant");
+  sdf_write_u16(file, argument.pointer, "argument pointer");
+  if (argument.pointer == SDF_POINTER_FLOAT_CONSTANT) {
+    sdf_write_f32(file, argument.float_constant, "argument float constant");
   }
+}
+
+static sdf_argument_t sdf__return_and_increment_written(void) {
+  sdf_argument_t output = sdf_argument_pointer(sdf__written);
+  sdf__written++;
+  return output;
 }
 
 void sdf_write_sdf_reset(void) {
   sdf__written = 0;
 }
 
-sdf_pointer_t sdf_write_sdf_nullary(
+sdf_argument_t sdf_write_sdf_nullary(
   FILE * file,
   sdf_opcode_t opcode
 ) {
   sdf_write_u16(file, opcode, "opcode");
-  return sdf__written++;
+  return sdf__return_and_increment_written();
 }
 
-sdf_pointer_t sdf_write_sdf_unary(
+sdf_argument_t sdf_write_sdf_unary(
   FILE * file,
   sdf_opcode_t opcode,
-  sdf_pointer_t argument_a_pointer,
-  sdf_f32_t argument_a_float_constant
+  sdf_argument_t argument_a
 ) {
   sdf_write_u16(file, opcode, "opcode");
-  sdf__write_argument(file, argument_a_pointer, argument_a_float_constant);
-  return sdf__written++;
+  sdf__write_argument(file, argument_a);
+  return sdf__return_and_increment_written();
 }
 
-sdf_pointer_t sdf_write_sdf_binary(
+sdf_argument_t sdf_write_sdf_binary(
   FILE * file,
   sdf_opcode_t opcode,
-  sdf_pointer_t argument_a_pointer,
-  sdf_f32_t argument_a_float_constant,
-  sdf_pointer_t argument_b_pointer,
-  sdf_f32_t argument_b_float_constant
+  sdf_argument_t argument_a,
+  sdf_argument_t argument_b
 ) {
   sdf_write_u16(file, opcode, "opcode");
-  sdf__write_argument(file, argument_a_pointer, argument_a_float_constant);
-  sdf__write_argument(file, argument_b_pointer, argument_b_float_constant);
-  return sdf__written++;
+  sdf__write_argument(file, argument_a);
+  sdf__write_argument(file, argument_b);
+  return sdf__return_and_increment_written();
 }
 
-sdf_pointer_t sdf_write_sdf_ternary(
+sdf_argument_t sdf_write_sdf_ternary(
   FILE * file,
   sdf_opcode_t opcode,
-  sdf_pointer_t argument_a_pointer,
-  sdf_f32_t argument_a_float_constant,
-  sdf_pointer_t argument_b_pointer,
-  sdf_f32_t argument_b_float_constant,
-  sdf_pointer_t argument_c_pointer,
-  sdf_f32_t argument_c_float_constant
+  sdf_argument_t argument_a,
+  sdf_argument_t argument_b,
+  sdf_argument_t argument_c
 ) {
   sdf_write_u16(file, opcode, "opcode");
-  sdf__write_argument(file, argument_a_pointer, argument_a_float_constant);
-  sdf__write_argument(file, argument_b_pointer, argument_b_float_constant);
-  sdf__write_argument(file, argument_c_pointer, argument_c_float_constant);
-  return sdf__written++;
+  sdf__write_argument(file, argument_a);
+  sdf__write_argument(file, argument_b);
+  sdf__write_argument(file, argument_c);
+  return sdf__return_and_increment_written();
 }
--- src/framework/write_sdf.h
+++ src/framework/write_sdf.h
@@ -3,40 +3,34 @@
 
 #include <stdio.h>
 #include "opcode.h"
-#include "pointer.h"
+#include "argument.h"
 
 void sdf_write_sdf_reset(void);
 
-sdf_pointer_t sdf_write_sdf_nullary(
+sdf_argument_t sdf_write_sdf_nullary(
   FILE * file,
   sdf_opcode_t opcode
 );
 
-sdf_pointer_t sdf_write_sdf_unary(
+sdf_argument_t sdf_write_sdf_unary(
   FILE * file,
   sdf_opcode_t opcode,
-  sdf_pointer_t argument_a_pointer,
-  sdf_f32_t argument_a_float_constant
+  sdf_argument_t argument_a
 );
 
-sdf_pointer_t sdf_write_sdf_binary(
+sdf_argument_t sdf_write_sdf_binary(
   FILE * file,
   sdf_opcode_t opcode,
-  sdf_pointer_t argument_a_pointer,
-  sdf_f32_t argument_a_float_constant,
-  sdf_pointer_t argument_b_pointer,
-  sdf_f32_t argument_b_float_constant
+  sdf_argument_t argument_a,
+  sdf_argument_t argument_b
 );
 
-sdf_pointer_t sdf_write_sdf_ternary(
+sdf_argument_t sdf_write_sdf_ternary(
   FILE * file,
   sdf_opcode_t opcode,
-  sdf_pointer_t argument_a_pointer,
-  sdf_f32_t argument_a_float_constant,
-  sdf_pointer_t argument_b_pointer,
-  sdf_f32_t argument_b_float_constant,
-  sdf_pointer_t argument_c_pointer,
-  sdf_f32_t argument_c_float_constant
+  sdf_argument_t argument_a,
+  sdf_argument_t argument_b,
+  sdf_argument_t argument_c
 );
 
 #endif
