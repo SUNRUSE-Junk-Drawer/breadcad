23550431b724e526fc1892e9ff1dbd77735540b1
jameswilddev <jameswilddev@outlook.com>
2019-10-19 09:21:06 +0100

    Implement translation transform (#3).

---
+++ src/executables/translate.c
@@ -0,0 +1,140 @@
+#define __USE_MINGW_ANSI_STDIO 1
+#include <stdio.h>
+#include "../framework/unused.h"
+#include "../framework/malloc.h"
+#include "../framework/types.h"
+#include "../framework/opcode.h"
+#include "../framework/pointer.h"
+#include "../framework/cli.h"
+#include "../framework/executable.h"
+#include "../framework/write_sdf.h"
+
+const char * sdf_executable_name = "translate";
+const char * sdf_executable_description = "translates the geometry described by a sdf stream";
+const char * sdf_executable_usage_prefix = "[sdf stream] | ";
+const char * sdf_executable_usage_suffix = " | [consumer of sdf stream]";
+const sdf_boolean_t sdf_executable_reads_model_from_stdin = SDF_BOOLEAN_TRUE;
+
+static sdf_f32_t sdf__translation[3];
+static sdf_boolean_t sdf__parameters_generated[] = { SDF_BOOLEAN_FALSE, SDF_BOOLEAN_FALSE, SDF_BOOLEAN_FALSE };
+static sdf_argument_t sdf__manipulated_parameters[3];
+
+void sdf_executable_cli(void) {
+  sdf_cli_float("x", "x", "translation on the x axis (millimeters)", &sdf__translation[0], 0.0f);
+  sdf_cli_float("y", "y", "translation on the y axis (millimeters)", &sdf__translation[1], 0.0f);
+  sdf_cli_float("z", "z", "translation on the z axis (millimeters)", &sdf__translation[2], 0.0f);
+}
+
+static sdf_pointer_t * sdf__remapped_pointers = NULL;
+static size_t sdf__read_instructions = 0;
+
+void sdf_executable_before_first_file(void) {
+}
+
+static void sdf__record_remapped_pointer(
+  sdf_argument_t argument
+) {
+  SDF_REALLOC(
+    sdf_pointer_t,
+    sdf__read_instructions + 1,
+    "record remapped pointers",
+    sdf__remapped_pointers
+  );
+  sdf__remapped_pointers[sdf__read_instructions] = argument.pointer;
+  sdf__read_instructions++;
+}
+
+static sdf_argument_t sdf__remap_argument(
+  sdf_argument_t argument
+) {
+  if (argument.pointer == SDF_POINTER_FLOAT_CONSTANT) {
+    return argument;
+  }
+
+  return sdf_argument_pointer(sdf__remapped_pointers[argument.pointer]);
+}
+
+void sdf_executable_nullary(
+  sdf_opcode_t opcode
+) {
+  sdf_opcode_id_t parameter;
+  sdf_argument_t original_parameter;
+  sdf_argument_t result;
+  if (opcode >= SDF_OPCODE_PARAMETER(0) && opcode <= SDF_OPCODE_PARAMETER(2)) {
+    parameter = sdf_opcode_id(opcode);
+    if (!sdf__parameters_generated[parameter]) {
+      original_parameter = sdf_write_sdf_nullary(opcode);
+      result = sdf_write_sdf_binary(
+        SDF_OPCODE_SUBTRACT,
+        original_parameter,
+        sdf_argument_float_constant(sdf__translation[parameter])
+      );
+      sdf__manipulated_parameters[parameter] = result;
+      sdf__parameters_generated[parameter] = SDF_BOOLEAN_TRUE;
+    } else {
+      result = sdf__manipulated_parameters[parameter];
+    }
+  } else {
+    result = sdf_write_sdf_nullary(opcode);
+  }
+  sdf__record_remapped_pointer(result);
+}
+
+void sdf_executable_unary(
+  sdf_opcode_t opcode,
+  sdf_argument_t argument_a
+) {
+  sdf_argument_t remapped_argument_a = sdf__remap_argument(argument_a);
+  sdf_argument_t result = sdf_write_sdf_unary(opcode, remapped_argument_a);
+  sdf__record_remapped_pointer(result);
+}
+
+void sdf_executable_binary(
+  sdf_opcode_t opcode,
+  sdf_argument_t argument_a,
+  sdf_argument_t argument_b
+) {
+  sdf_argument_t remapped_argument_a = sdf__remap_argument(argument_a);
+  sdf_argument_t remapped_argument_b = sdf__remap_argument(argument_b);
+  sdf_argument_t result = sdf_write_sdf_binary(
+    opcode,
+    remapped_argument_a,
+    remapped_argument_b
+  );
+  sdf__record_remapped_pointer(result);
+}
+
+void sdf_executable_ternary(
+  sdf_opcode_t opcode,
+  sdf_argument_t argument_a,
+  sdf_argument_t argument_b,
+  sdf_argument_t argument_c
+) {
+  sdf_argument_t remapped_argument_a = sdf__remap_argument(argument_a);
+  sdf_argument_t remapped_argument_b = sdf__remap_argument(argument_b);
+  sdf_argument_t remapped_argument_c = sdf__remap_argument(argument_c);
+  sdf_argument_t result = sdf_write_sdf_ternary(
+    opcode,
+    remapped_argument_a,
+    remapped_argument_b,
+    remapped_argument_c
+  );
+  sdf__record_remapped_pointer(result);
+}
+
+void sdf_executable_eof(void) {
+}
+
+void sdf_executable_after_last_file(void) {
+}
+
+sdf_f32_t sdf_executable_get_parameter(
+  void * parameter_context,
+  size_t iteration,
+  sdf_opcode_id_t id
+) {
+  SDF_UNUSED(parameter_context);
+  SDF_UNUSED(iteration);
+  SDF_UNUSED(id);
+  return 0.0f;
+}
---
+++ test/executables/translate.bash
@@ -0,0 +1,100 @@
+load "../framework/main"
+
+executable_name=translate
+executable_help="translate - translates the geometry described by a sdf stream
+  usage: [sdf stream] | translate [options] | [consumer of sdf stream]
+  options:
+    -h, --help, /?: display this message
+    -x [number], --x [number]: translation on the x axis (millimeters) (default: 0.000000)
+    -y [number], --y [number]: translation on the y axis (millimeters) (default: 0.000000)
+    -z [number], --z [number]: translation on the z axis (millimeters) (default: 0.000000)"
+
+@test "help (short name)" {
+  test_help "-h"
+}
+
+@test "help (long name)" {
+  test_help "--help"
+}
+
+@test "help (query)" {
+  test_help "/?"
+}
+
+@test "empty stdin" {
+  check_successful "bin/translate < test/sdf/empty.sdf | bin/sample" "inf"
+}
+
+function test_cuboid {
+  parameters=$1
+
+  x_origin=$2
+  x_low=$3
+  x_high=$4
+  y_origin=$5
+  y_low=$6
+  y_high=$7
+  z_origin=$8
+  z_low=$9
+  z_high=${10}
+
+  check_successful "bin/cuboid --size-x 2 --size-y 8 --size-z 4 | bin/translate $parameters | bin/sample -x $x_origin -y $y_origin -z $z_origin" "-0.500000"
+  check_successful "bin/cuboid --size-x 2 --size-y 8 --size-z 4 | bin/translate $parameters | bin/sample -x $x_low -y $y_origin -z $z_origin" "-0.400000"
+  check_successful "bin/cuboid --size-x 2 --size-y 8 --size-z 4 | bin/translate $parameters | bin/sample -x $x_high -y $y_origin -z $z_origin" "-0.400000"
+  check_successful "bin/cuboid --size-x 2 --size-y 8 --size-z 4 | bin/translate $parameters | bin/sample -x $x_origin -y $y_low -z $z_origin" "-0.400000"
+  check_successful "bin/cuboid --size-x 2 --size-y 8 --size-z 4 | bin/translate $parameters | bin/sample -x $x_origin -y $y_high -z $z_origin" "-0.400000"
+  check_successful "bin/cuboid --size-x 2 --size-y 8 --size-z 4 | bin/translate $parameters | bin/sample -x $x_origin -y $y_origin -z $z_low" "-0.400000"
+  check_successful "bin/cuboid --size-x 2 --size-y 8 --size-z 4 | bin/translate $parameters | bin/sample -x $x_origin -y $y_origin -z $z_high" "-0.400000"
+}
+
+@test "default" {
+  test_cuboid "" "0.5" "0.4" "1.6" "0.5" "0.4" "7.6" "0.5" "0.4" "3.6"
+}
+
+@test "x short" {
+  test_cuboid "-x 10" "10.5" "10.4" "11.6" "0.5" "0.4" "7.6" "0.5" "0.4" "3.6"
+}
+
+@test "y short" {
+  test_cuboid "-y 20" "0.5" "0.4" "1.6" "20.5" "20.4" "27.6" "0.5" "0.4" "3.6"
+}
+
+@test "z short" {
+  test_cuboid "-z 25" "0.5" "0.4" "1.6" "0.5" "0.4" "7.6" "25.5" "25.4" "28.6"
+}
+
+@test "x y short" {
+  test_cuboid "-x 10 -y 20" "10.5" "10.4" "11.6" "20.5" "20.4" "27.6" "0.5" "0.4" "3.6"
+}
+
+@test "x z short" {
+  test_cuboid "-x 10 -z 25" "10.5" "10.4" "11.6" "0.5" "0.4" "7.6" "25.5" "25.4" "28.6"
+}
+
+@test "y z short" {
+  test_cuboid "-y 20 -z 25" "0.5" "0.4" "1.6" "20.5" "20.4" "27.6" "25.5" "25.4" "28.6"
+}
+
+@test "x long" {
+  test_cuboid "--x 10" "10.5" "10.4" "11.6" "0.5" "0.4" "7.6" "0.5" "0.4" "3.6"
+}
+
+@test "y long" {
+  test_cuboid "--y 20" "0.5" "0.4" "1.6" "20.5" "20.4" "27.6" "0.5" "0.4" "3.6"
+}
+
+@test "z long" {
+  test_cuboid "--z 25" "0.5" "0.4" "1.6" "0.5" "0.4" "7.6" "25.5" "25.4" "28.6"
+}
+
+@test "x y long" {
+  test_cuboid "--x 10 --y 20" "10.5" "10.4" "11.6" "20.5" "20.4" "27.6" "0.5" "0.4" "3.6"
+}
+
+@test "x z long" {
+  test_cuboid "--x 10 --z 25" "10.5" "10.4" "11.6" "0.5" "0.4" "7.6" "25.5" "25.4" "28.6"
+}
+
+@test "y z long" {
+  test_cuboid "--y 20 --z 25" "0.5" "0.4" "1.6" "20.5" "20.4" "27.6" "25.5" "25.4" "28.6"
+}
