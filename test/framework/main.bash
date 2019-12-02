a32e458c3c1a4a6042a22a4706273956a8d358c7
jameswilddev <jameswilddev@outlook.com>
2019-12-02 23:32:38 +0000

    Add a temporary directory which will be used to work around Windows process substitution failures (#69).

--- test/framework/main.bash
+++ test/framework/main.bash
@@ -3,3 +3,11 @@ load "../../submodules/bats-assert/load"
 load "../framework/assertions"
 load "../framework/help"
 load "../framework/number_parameter"
+
+setup() {
+  mkdir -p temp
+}
+
+teardown() {
+  rm -r temp
+}
