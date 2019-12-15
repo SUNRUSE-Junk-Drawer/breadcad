b1d7c82de8c1baf33983c912515520337c251638
jameswilddev <jameswilddev@outlook.com>
2019-12-15 01:35:02 +0000

    Checkout the specific tag being built, then ensure that tag is repointed before push to mirror (#25).

--- php/split.bash
+++ php/split.bash
@@ -5,15 +5,17 @@ set -x
 
 cd `mktemp -d`
 git clone https://github.com/jameswilddev/breadcad .
-git tag --annotate $TRAVIS_TAG --message $TRAVIS_TAG --force
+git checkout $TRAVIS_TAG
+git checkout -b release-temp
 rm -f php/splitsh-lite.tar.gz
 wget -O php/splitsh-lite.tar.gz https://github.com/splitsh/lite/releases/download/v1.0.1/lite_linux_amd64.tar.gz
 tar -C php -xvzf php/splitsh-lite.tar.gz
 
 function split() {
   git remote add $1 "https://$GITHUB_TOKEN@github.com/jameswilddev/breadcad-$1"
+  git checkout release-temp
   SHA1=`php/splitsh-lite --prefix=$2`
-
+  git tag --annotate $TRAVIS_TAG --message $TRAVIS_TAG --force $SHA1
   git push $1 "$SHA1:refs/heads/master" --tags --force
 }
 
