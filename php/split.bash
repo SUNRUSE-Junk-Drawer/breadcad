966e6d13aa678c4cda206b24dd8c97911fe6df64
jameswilddev <jameswilddev@outlook.com>
2019-12-15 00:58:23 +0000

    See whether tags can be published (#25).

--- php/split.bash
+++ php/split.bash
@@ -4,7 +4,7 @@ set -e
 set -x
 
 cd `mktemp -d`
-git clone https://github.com/jameswilddev/breadcad .
+git clone --depth 1 --single-branch --branch $TRAVIS_TAG https://github.com/jameswilddev/breadcad .
 rm -f php/splitsh-lite.tar.gz
 wget -O php/splitsh-lite.tar.gz https://github.com/splitsh/lite/releases/download/v1.0.1/lite_linux_amd64.tar.gz
 tar -C php -xvzf php/splitsh-lite.tar.gz
@@ -12,7 +12,8 @@ tar -C php -xvzf php/splitsh-lite.tar.gz
 function split() {
   git remote add $1 "https://$GITHUB_TOKEN@github.com/jameswilddev/breadcad-$1"
   SHA1=`php/splitsh-lite --prefix=$2`
-  git push $1 "$SHA1:refs/heads/master"
+
+  git push $1 "$SHA1:refs/heads/master" --tags
 }
 
 split "framework" "php/framework"
