189cd872ab8c15ba6d5acd1f88cadf6553767050
jameswilddev <jameswilddev@outlook.com>
2019-12-15 01:22:23 +0000

    Update tags before push (#25).

--- php/split.bash
+++ php/split.bash
@@ -5,6 +5,7 @@ set -x
 
 cd `mktemp -d`
 git clone https://github.com/jameswilddev/breadcad .
+git tag --annotate $TRAVIS_TAG --message $TRAVIS_TAG --force
 rm -f php/splitsh-lite.tar.gz
 wget -O php/splitsh-lite.tar.gz https://github.com/splitsh/lite/releases/download/v1.0.1/lite_linux_amd64.tar.gz
 tar -C php -xvzf php/splitsh-lite.tar.gz
@@ -13,7 +14,7 @@ function split() {
   git remote add $1 "https://$GITHUB_TOKEN@github.com/jameswilddev/breadcad-$1"
   SHA1=`php/splitsh-lite --prefix=$2`
 
-  git push $1 "$SHA1:refs/heads/master" --tags
+  git push $1 "$SHA1:refs/heads/master" --tags --force
 }
 
 split "framework" "php/framework"
